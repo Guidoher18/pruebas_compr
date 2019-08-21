2d894221711243be639f17506e918111e7fea483
unknown <guido_181193@outlook.com>
2019-08-21 19:59:44 -0300

    Keypress entrada de solo numeros

--- Digitos.js
+++ Digitos.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     var Numero_de_Tarea = 0; //Permite seguir el Itinerario
     var Serie = 0;
     
-    var Conteo_de_Clic_en_Bloque = 0;
+    var Conteo_de_Tecleo = 0;
     
     var Errores = 0;
     var Respuesta1 = "";
@@ -28,6 +28,42 @@ $(document).ready(function(){
     var TRc = '';
     var TRd = '';
 
+    function muestro_tecla (tecla){ //Permite mostrar la tecla presionada, sumado a las ya presionadas
+        var a = $('#Marco>p').html() + tecla;
+        $('#Marco>p').html(a);
+        Conteo_de_Tecleo += 1;
+    }
     
+        
+    $(document).keypress(function(e){
+        if (Conteo_de_Tecleo < 100 /* AGREGAR SERIE!!!!!!!!!!!!*/){
+            switch(e.keyCode)
+            {
+                case 48: muestro_tecla(e.key)
+                break;
+                case 49: muestro_tecla(e.key)
+                break;
+                case 50: muestro_tecla(e.key)
+                break;
+                case 51: muestro_tecla(e.key)
+                break;
+                case 52: muestro_tecla(e.key)
+                break;
+                case 53: muestro_tecla(e.key)
+                break;
+                case 54: muestro_tecla(e.key)
+                break;
+                case 55: muestro_tecla(e.key)
+                break;
+                case 56: muestro_tecla(e.key)
+                break;
+                case 57: muestro_tecla(e.key)
+                break;
+                case 58: muestro_tecla(e.key)
+                break;
+                default:break;                  
+            };
+        }
+    })
 
 });
